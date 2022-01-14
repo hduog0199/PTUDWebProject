eb8571c17322b3576e3d6eeb7d4158ae41695625
hieule <vanhieu2241998@gmail.com>
2022-01-14 11:31:47 +0700

    test branch

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 //setting view engine here for project
 const express = require('express');
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
 app.engine('.hbs', exphbs({
     extname: '.hbs',
-    layoutsDir:'views/_layouts',
-    partialsDir:'views/_partials'
+    layoutsDir: 'views/_layouts',
+    partialsDir: 'views/_partials'
 }));
 app.set('view engine', '.hbs');
 
 //declare middleware with routes (as controller in MVC model)
 
-app.use('/admin',require('./routes/user.route'));
-app.use('/admin',require('./routes/ndql.route'));
+app.use('/admin', require('./routes/user.route'));
+app.use('/admin', require('./routes/ndql.route'));
 
-// async function main(){
-//     const dbcv=require('./utils/dbCovid');
-//     var str=`select * from "User"`
-//     var rs= await dbcv.load(str);
-//     console.log(rs);
-// }
-// main();
+async function main() {
+    const dbcv = require('./utils/dbCovid');
+    var str = `select * from "User"`
+    var rs = await dbcv.load(str);
+    console.log(rs);
+}
+main();
 
 
 // default handle and do not edit here.Thank you
@@ -30,8 +30,7 @@ app.get('/', function (req, res) {
     res.render('home');
 });
 
-const PORT=3000;
-app.listen(PORT,function()
-{
+const PORT = 3000;
+app.listen(PORT, function () {
     console.log(`Server is listened at http://localhost:${PORT}`)
 })
